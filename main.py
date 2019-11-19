ea51b5e0a4c1e8878e814fb8c039f3f338f1fc76
Kyrillos_Akram <kerillos.akram@gmail.com>
2019-11-19 23:05:32 +0200

    reduce libs ,cleaning code

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from playsound import playsound
 from kivy.properties import ObjectProperty
 from kivy.uix.recycleview import RecycleView
 
 #from kivy.uix.widget import Butt
 
 class lay(BoxLayout):
-    ''',RecycleViewdef __init__(**kwarg):
-        super(lay,self).__init__(**kwarg)
-        self.data=[{'text':str(x)} for x in range(30)]'''
     theinput=ObjectProperty()
 
     def calculate(self,calculation):
@@ -20,14 +16,10 @@ class lay(BoxLayout):
         except:
             return 'Error'
 
-#    def woof(self):
-#        playsound("Dog-Woof-SoundBible.com-457935112.mp3")
-#        print("woof !!\n")
-
 
 class calculatorApp(App):
     title="Calculator"
-    icon="icon-picture-1.jpg"
+    #icon='/mnt/369071E49071AB4F/MyLab/Space/project_box/training/calculator_in_kivy/icon-picture-1.ico'
 
 if __name__=='__main__':
     calculatorApp().run()
