9f5d7956a535c63fd6cf67abc742aeaf942b320f
Kyrillos_Akram <kerillos.akram@gmail.com>
2019-11-19 12:49:50 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from playsound import playsound
+from kivy.properties import ObjectProperty
+from kivy.uix.recycleview import RecycleView
+
+#from kivy.uix.widget import Butt
+
+class lay(BoxLayout):
+    ''',RecycleViewdef __init__(**kwarg):
+        super(lay,self).__init__(**kwarg)
+        self.data=[{'text':str(x)} for x in range(30)]'''
+    theinput=ObjectProperty()
+#    def woof(self):
+#        playsound("Dog-Woof-SoundBible.com-457935112.mp3")
+#        print("woof !!\n")
+
+
+class calculatorApp(App):
+    title="Calculator"
+    icon="icon-picture-1.jpg"
+
+if __name__=='__main__':
+    calculatorApp().run()
